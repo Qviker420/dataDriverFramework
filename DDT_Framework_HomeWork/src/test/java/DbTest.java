fe50aea12d5a0ac1f463f269a55cc1d132a194b4
user1 <chaligavaduta3@gmail.com>
2023-05-23 17:20:15 +0400

    DDT

---
+++ DDT_Framework_HomeWork/src/test/java/DbTest.java
@@ -0,0 +1,55 @@
+import com.codeborne.selenide.SelenideElement;
+import org.example.MySqlJDCButil;
+import org.openqa.selenium.By;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class DbTest {
+
+    @BeforeTest
+    public void setup() {
+        try (Connection conn = MySqlJDCButil.getConnection()) {
+            System.out.println(String.format("Connected to database %s "
+                    + "successfully.", conn.getCatalog()));
+        }
+        catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+    }
+
+    @Test
+    public void dbTest() throws SQLException {
+        open("https://demoqa.com/automation-practice-form");
+        SelenideElement firstName = $(By.id("firstName"));
+        SelenideElement lastName = $(By.id("lastName"));
+        SelenideElement phoneNumber = $(By.id("userNumber"));
+
+        String sql = "SELECT firstname, lastname, phone" +
+                "FROM students";
+
+        try (Connection conn = MySqlJDCButil.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                firstName.setValue(rs.getString("firstname"));
+                lastName.setValue(rs.getString("firstname"));
+                phoneNumber.setValue(rs.getString("firstname"));
+
+                firstName.clear();
+                lastName.clear();
+                phoneNumber.clear();
+            }
+        }
+    }
+}
+
